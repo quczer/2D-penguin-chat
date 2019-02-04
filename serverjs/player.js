085fc2050bd4372664e359757524bab0f5b04138
quczer <kucharczi@gmail.com>
2019-02-04 20:29:32 +0100

    wyrzuciłem troche rzeczy

--- serverjs/player.js
+++ serverjs/player.js
@@ -1,23 +1,14 @@
 const process = require('process');
-
-const respawnTime = 7;
-
 function getLucky(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function Player(name, ctr) {
-    this.ctr = ctr;
+function Player(name) {
     this.name = name;
     this.x = getLucky(0 + 200, 1920 - 200);
     this.y = getLucky(0 + 200, 1200 - 200);
-    this.angle = getLucky(0, 3.1415 * 2);
     this.speed = 200;
     this.lastTime = Date.now();
-    this.lastTimeShoot = Date.now();
-    this.shootSpeed = 0.2;
-    this.health = 100;
-    this.isAlive = true;
     this.lastChat= {
         data:'',
         killtime: 0
@@ -28,40 +19,9 @@ function Player(name, ctr) {
     }
 
     this.act = function (mov) {
-        let dt = (Date.now() - this.lastTime) / 1000.0;
-        this.lastTime = Date.now();
-
-        if (this.isAlive === false) {
-            if (this.respawnCounter <= respawnTime) {
-                this.respawnCounter += dt;
-                return false;
-            }
-            else {
-                //console.log(`respawn ${this.name}`);
-                this.respawnCounter = 0;
-                this.isAlive = true;
-                this.x = getLucky(0 + 200, 1920 - 200);
-                this.y = getLucky(0 + 200, 1200 - 200);
-                this.angle = getLucky(0, 3.1415 * 2);
-                return `${this.name} has respawned`;
-            }
-        }
-
         let speed = 10;
-        let angle = 0;
-       
-        this.angle = Math.PI * 0.5;
         this.x += speed * (mov.right ? (mov.left ? 0 : 1) : (mov.left ? -1 : 0));
         this.y += speed * (mov.down ? (mov.up ? 0 : 1) : (mov.up ? -1 : 0));
-        return false;
-    }
-
-    this.tryToShoot = function () {
-        let dt = (Date.now() - this.lastTimeShoot) / 1000.0;
-        if (dt > this.shootSpeed) {
-            this.lastTimeShoot = Date.now();
-            return true;
-        }
         return false;
     }
 }
